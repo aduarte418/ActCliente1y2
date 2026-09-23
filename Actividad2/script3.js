657e46e82b3ac9d484e74ffd3f2594ac596f752d
antondm11 <antondm11@gmail.com>
2026-09-23 20:04:40 +0200

    Create scripts

---
+++ Actividad2/script3.js
@@ -0,0 +1,8 @@
+
+// Bucle for vacío para simular script pesado
+for(let i = 0; i < 50000000; i++){
+
+}
+
+// Línea de código para cambiar texto
+document.getElementById('titulo').innerText = 'Actividad de Antonio';
